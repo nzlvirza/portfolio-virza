e30ce1259ce72c1de81900e9ee010e2c7bfc69b8
Nazla Virza Rahman <virzarahman@gmail.com>
2026-08-27 03:40:48 +0700

    fix: serve static files directly from api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,30 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . '/..' . $uri;
+
+if ($uri !== '/' && is_file($file)) {
+    $mimeTypes = [
+        'css'  => 'text/css',
+        'js'   => 'application/javascript',
+        'json' => 'application/json',
+        'png'  => 'image/png',
+        'jpg'  => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'gif'  => 'image/gif',
+        'svg'  => 'image/svg+xml',
+        'ico'  => 'image/x-icon',
+        'woff' => 'font/woff',
+        'woff2'=> 'font/woff2',
+        'ttf'  => 'font/ttf',
+        'pdf'  => 'application/pdf',
+    ];
+    $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+    if (isset($mimeTypes[$ext])) {
+        header('Content-Type: ' . $mimeTypes[$ext]);
+        readfile($file);
+        exit;
+    }
+}
+
+require __DIR__ . '/../public/index.php';
