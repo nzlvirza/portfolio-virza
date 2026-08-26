0320f0ee09fc03cee3c9d8f16d9ecdbda140db77
Nazla Virza Rahman <virzarahman@gmail.com>
2026-08-26 21:26:40 +0700

    Configure Laravel Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [tailwindcss()],
-    base: './',
-    build: {
-        outDir: 'dist',
-    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
